88144c13440547156d0e489e5093c9a91ef87d8a
rootx <cslroot@gmail.com>
2022-01-31 14:07:12 +0900

    add: test for core

---
+++ test/core/TestTopology.cpp
@@ -0,0 +1,15 @@
+#include <cs/base.h>
+#include <cs/core/topo.h>
+#include <stdexcept>
+#include <gtest/gtest.h>
+
+using namespace cs;
+using namespace cs::core;
+
+
+TEST(TestTopology, GetType)
+{
+    Vertex* v = CS_NEW Vertex();
+    EXPECT_EQ(v->GetType(), TopologyType::Vertex);
+}
+
