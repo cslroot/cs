4d77bed34c5c25a30afac12ec55fbda5ebe1924b
@rootx <cslroot@gmail.com>
2022-05-02 08:39:15 +0900

    refactor render

--- src/render/include/cs/render.h
+++ src/render/include/cs/render.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <cs/render/render_common.h>
-#include <cs/render/Renderer.h>
 #include <cs/render/EmptyRenderer.h>
 #include <cs/render/GLFWRenderer.h>
+#include <cs/render/Renderer.h>
+#include <cs/render/render_common.h>
 
-#include <cs/render/camera.h>
-#include <cs/render/material.h>
 #include <cs/render/CSDisplayableObject.h>
-#include <cs/render/scene.h>
-#include <cs/render/mesh.h>
+#include <cs/render/Camera.h>
+#include <cs/render/Material.h>
+#include <cs/render/Mesh.h>
+#include <cs/render/Scene.h>
