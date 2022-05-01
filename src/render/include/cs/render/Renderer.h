3c45a5c0cd8c8ffc649db95b1e92e59c84b3eb9d
@rootx <cslroot@gmail.com>
2022-05-01 21:56:34 +0900

    fix case of filename

---
+++ src/render/include/cs/render/Renderer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cs/render/render_common.h>
+#include <cs/render_fwd.h>
+
+namespace cs
+{
+    namespace render
+    {
+        class Renderer
+        {
+        private:
+            /* data */
+        public:
+            DECL_CS_RENDER Renderer(/* args */);
+            DECL_CS_RENDER virtual ~Renderer();
+
+        public:
+            DECL_CS_RENDER virtual void Init() {}
+            DECL_CS_RENDER virtual void Terminate() {}
+            DECL_CS_RENDER virtual void Render(const Scene& scene, const Camera& camera);
+            DECL_CS_RENDER virtual void Resize(int width, int height);
+            DECL_CS_RENDER virtual bool WindowShouldClose() { return false; }
+
+        private:
+            int _width;
+            int _height;
+
+        };
+
+    } // namespace render
+
+} // namespace cs
